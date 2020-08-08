5a00fec400d28691a7e7f1875215b610c2e0e857
yifang0-0 <51746239+yifang0-0@users.noreply.github.com>
2020-08-09 01:23:32 +0800

    在wxml里设置高度失败的迷惑行为解决 大概是在onready的时候获得的数字不能用来设在界面里

--- miniprogram/pages/release-series/release-series.js
+++ miniprogram/pages/release-series/release-series.js
@@ -4,6 +4,9 @@ Page({
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     hidden: true,
+    indexS:0,
+    topBarSeriesHeight:171,
+    
     SeriesList:[
       {
         title: '气垫',
@@ -46,30 +49,96 @@ Page({
       }
     ]
   },
-  onLoad() {
-    var query = wx.createSelectorQuery();
+  onReady: 
+    function() {
+      let that = this;
+      setTimeout(function() {
+        that.calculateScrollViewHeight()
+      }, 100)
+      //calculateScrollViewHeight();
+    },
+    //     //console.log(e)
+    // wx.hideLoading()
+    //     let that = this
 
-        //选择id
+    //     let query = wx.createSelectorQuery()
+    
+    //     query.selectAll("#seriesLable1").boundingClientRect(function (res) {
+    
+    //       console.log(res[0])
+    //       var a=res[0].height
+    //       console.log("当前坐标是 position = "+a)
+    
+  
+    //       that.setData({
+      
+    //       topBarSeriesHeight:a,
+    //         
+    //       })
+    //       console.log("topBarSeriesHeight= "+topBarSeriesHeight)
+    
+    //     }).exec()
+      
+    calculateScrollViewHeight() {
+      let that = this;
+     // console.log(systemInfo)
+      let query = wx.createSelectorQuery().in(this)
+      //根据节点id查询节点部分的高度（px单位）
+      query.select('#topBarSeries').boundingClientRect();
+      query.select('#seriesLable1').boundingClientRect();
 
-        query.select('topBarSeries').boundingClientRect()
-        
-       query.select('seriesLable').boundingClientRect()
-
-        query.exec(function (res) {
-
-          //res就是 所有标签为mjltest的元素的信息 的数组
-
-          console.log('所有：',res);
-
-          //取高度
-
-          that.setData({
-
-            storyHeight:that.data.detailHeight -(res[0].height+res[1].height)
-
-          })
-
-        });
-
+      
+  
+      query.exec((res) => {
+        // 分别取出节点的高度
+        let bar = res[0].height;
+        let imageHeight = res[1].height;
+        // 然后窗口高度（wx.getSystemInfoSync().windowHeight）减去其他不滑动界面的高度
+        let scrollViewHeight = wx.getSystemInfoSync().windowHeight -
+         imageHeight-bar ;
+        console.log(scrollViewHeight)
+        // 算出来之后存到data对象里面
+        that.setData({
+          scrollHeight: scrollViewHeight
+        });
+      })
+    },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      MainCur: e.currentTarget.dataset.id,
+      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+    })
+  },
+  VerticalMain(e) {
+    let that = this;
+    let SeriesList = this.data.SeriesList;
+    let tabHeight = 0;
+    if (this.data.load) {
+      for (let i = 0; i < SeriesList.length; i++) {
+        let view = wx.createSelectorQuery().select("#series-" + i);
+        view.fields({
+          size: true
+        }, data => {
+          SeriesList[i].top = tabHeight;
+          tabHeight = tabHeight + data.height;
+          SeriesList[i].bottom = tabHeight;     
+        }).exec();
+      }
+      that.setData({
+        load: false,
+        SeriesList: SeriesList
+      })
+    }
+    let scrollTop = e.detail.scrollTop + 20;
+    for (let i = 0; i < SeriesList.length; i++) {
+      if (scrollTop > SeriesList[i].top && scrollTop < SeriesList[i].bottom) {
+        that.setData({
+          VerticalNavTop: (SeriesList[i].id - 1) * 50,
+          TabCur: SeriesList[i].id
+        })
+        return false
+      }
+    }
   }
-});
+   });
